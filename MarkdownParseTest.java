1eb1b4a9b1ee7893870faa3e73f6b3cc643fe552
NateWick4 <103294546+NateWick4@users.noreply.github.com>
2022-05-22 20:44:57 -0700

    added labrep 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -34,8 +34,8 @@ public class MarkdownParseTest {
     @Test
     public void reportEightOneTest() throws IOException{
         ArrayList<String> expected = new ArrayList();
-        expected.add("google.com")
-        expected.add("ucsd.edu")
+        expected.add("google.com");
+        expected.add("ucsd.edu");
         Path fileName = Path.of("test-file-rep1.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
@@ -44,8 +44,8 @@ public class MarkdownParseTest {
     @Test
     public void reportEightTwoTest() throws IOException{
         ArrayList<String> expected = new ArrayList();
-        expected.add("a.com(())")
-        expected.add("example.com")
+        expected.add("a.com(())");
+        expected.add("example.com");
         Path fileName = Path.of("test-file-rep2.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
@@ -54,7 +54,7 @@ public class MarkdownParseTest {
     @Test
     public void reportEightThreeTest() throws IOException{
         ArrayList<String> expected = new ArrayList();
-        expected.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule")
+        expected.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
         Path fileName = Path.of("test-file-rep3.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);

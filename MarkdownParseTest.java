16f9caf2a581a7b9bfc0efb9e6b696277bf39f4f
NateWick4 <103294546+NateWick4@users.noreply.github.com>
2022-05-22 20:12:50 -0700

    added tests for labrep 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -31,6 +31,33 @@ public class MarkdownParseTest {
         ArrayList<String> links = MarkdownParse.getLinks(content);
         assertEquals(expected, links);
     }
-
-
+    @Test
+    public void reportEightOneTest() throws IOException{
+        ArrayList<String> expected = new ArrayList();
+        expected.add("google.com")
+        expected.add("ucsd.edu")
+        Path fileName = Path.of("test-file-rep1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(expected, links);
+    }
+    @Test
+    public void reportEightTwoTest() throws IOException{
+        ArrayList<String> expected = new ArrayList();
+        expected.add("a.com(())")
+        expected.add("example.com")
+        Path fileName = Path.of("test-file-rep2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(expected, links);
+    }
+    @Test
+    public void reportEightThreeTest() throws IOException{
+        ArrayList<String> expected = new ArrayList();
+        expected.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule")
+        Path fileName = Path.of("test-file-rep3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(expected, links);
+    }
 }
